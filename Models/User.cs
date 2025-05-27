6d02965445937a9dc165e7ff41061822593d2bf2
Maria Alice <nantesmalice@gmail.com>
2025-05-27 16:49:59 -0400

    Adding user model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+public class User
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public Address Address { get; private set; }
+
+    public User(string name, Address address)
+    {
+        Name = name;
+        Address = address;
+    }
+}
