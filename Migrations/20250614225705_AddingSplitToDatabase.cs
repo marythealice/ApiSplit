e1b9e89ac24ea1f407ed0dbb369afc014288db3c
Maria Alice <nantesmalice@gmail.com>
2025-07-10 17:50:46 -0400

    adding unique index to user (email)

---
+++ Migrations/20250614225705_AddingSplitToDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ApiSplit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSplitToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Splits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    BottleId = table.Column<long>(type: "bigint", nullable: false),
+                    Volume = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    PayDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Splits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Splits_Bottles_BottleId",
+                        column: x => x.BottleId,
+                        principalTable: "Bottles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Splits_BottleId",
+                table: "Splits",
+                column: "BottleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Splits");
+        }
+    }
+}
